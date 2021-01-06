f8c5a5282b4f6e12edd9e0aadd1df1d604b5436d
jwang707 <jwang2367@gmail.com>
2021-01-05 22:53:36 -0500

    Part II done and added lowercasing of everything

--- PigLatin.java
+++ PigLatin.java
@@ -3,26 +3,38 @@ public class PigLatin{
   public static void main(String[] args){
     Scanner n = new Scanner(System.in);
     while (n.hasNextLine()){
-      Scanner line = new Scanner (n.nextLine());
+      Scanner line = new Scanner(n.nextLine());
       while (line.hasNext()){
-        System.out.print(pigLatinSimple(line.next()));
+        System.out.print(pigLatin(line.next()));
     }
     System.out.println();
   }
 }
 
-  public static String pigLatinSimple(String s){ //changes an individual s to pigLatinSimple
-    String result = "";
-  /*  while (n.hasNextLine()){
-      Scanner line = new Scanner (n.nextLine());
-      while (line.hasNext()){ */
-        if (s.charAt(0) == 'a' || s.charAt(0) == 'e' || s.charAt(0) == 'i' || s.charAt(0) == 'o' || s.charAt(0) == 'u') {
-          return s + "hay ";
-        }
-        else{
-          return s.substring(1, s.length()) + s.substring(0, 1) + "ay ";
-        }
-      //}
-  //  }
+  public static String pigLatinSimple(String s){ //changes an individual word to pigLatinSimple
+    String word = s.toLowerCase();
+    if (word.charAt(0) == 'a' || word.charAt(0) == 'e' || word.charAt(0) == 'i' || word.charAt(0) == 'o' || word.charAt(0) == 'u') {
+      return word + "hay ";
+    }
+    else{
+      return word.substring(1, word.length()) + word.substring(0, 1) + "ay ";
+    }
   }
+
+  public static String pigLatin(String s){ //changes an individual word to pigLatin
+    String word = s.toLowerCase();
+    String[] digraphs = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+    if (word.charAt(0) == 'a' || word.charAt(0) == 'e' || word.charAt(0) == 'i' || word.charAt(0) == 'o' || word.charAt(0) == 'u') {
+      return word + "hay ";
+    }
+    if (word.length() >= 2){
+      for (String digraph : digraphs){
+        if (word.substring(0, 2).equals(digraph)) {
+          return word.substring(2, word.length()) + word.substring(0, 2) + "ay ";
+        }
+      }
+    }
+    return word.substring(1, word.length()) + word.substring(0, 1) + "ay ";
+  }
+
 }
